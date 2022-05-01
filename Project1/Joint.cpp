1faa63c03f6f32a8a3c7e58f9629e9da2f33b170
MARIANOCEREDA <gmarianocereda@gmail.com>
2022-05-01 21:27:25 +0200

    Creacion de archivos de cabecera y clases basicas

---
+++ Project1/Joint.cpp
@@ -0,0 +1,19 @@
+
+#include "Joint.h"
+
+Joint::Joint(int num,string type)
+{
+	this->num = num;
+	this->type = type;
+}
+
+float Joint::getValue()
+{
+	return this->value;
+}
+
+void Joint::setValue(float value)
+{
+	this->value = value;
+}
+
