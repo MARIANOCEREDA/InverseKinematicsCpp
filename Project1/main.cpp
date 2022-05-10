24db7123eb06a3528f2c45704706c40b2fefefb3
MARIANOCEREDA <gmarianocereda@gmail.com>
2022-05-10 19:19:13 +0200

    Pruebas en main

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,6 +1,6 @@
 
 #include "./headers/Robot.h"
-#include "headers/MatrixOperations.h"
+#include "./headers/Matrix.h"
 #include <cmath>
 
 #define M_PI 3.141592653589793238462643383279502884
@@ -13,16 +13,16 @@ int main(int argc, char* argv[], char* envp[]) {
 	double base[4][4] = { {1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1} };
 
 	//Denavit-Hartenberg Matrix
-	double dh[6][4] = { {0,0.575,0.175,M_PI / 2},{0,0,0.89,0},{0,0,M_PI / 2,0},{1.05,0,-M_PI / 2,0},{0,0,0,M_PI / 2},{0,0.185,0,0} };
+	double dh[6][4] = { {0,0.575,0.175,M_PI/2},{0,0,0.89,0},{0,0,M_PI / 2,0},{1.05,0,-M_PI / 2,0},{0,0,0,M_PI / 2},{0,0.185,0,0} };
 
 	//T Matrix
 	double T[4][4] = { {0.7959,0.2612,0.5461,0.6795},{0.0177,0.8917,-0.4522,0.6699},{-0.6051,0.3696,0.7051,2.6586},{0,0,0,1} };
 
 	double mprod[4][4];
-	Matrix T1(T);
-	Matrix T2(base);
-	Matrix result;
-	result = T2*T1;
+	Matrix T1(tool);
+	Matrix T2(T);
+	Matrix result(T2*T1);
 	result.print();
 	system("pause");
 }
+
